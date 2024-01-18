1aa25deb2cd713692a456f6a1dcf2525e2f49909
Vlad Muslimov <muslimov.vlad@mail.ru>
2024-01-18 15:28:58 +0500

    initial commit

---
+++ src/main/java/muslimov/vlad/gbspring/GbSpringApplication.java
@@ -0,0 +1,13 @@
+package muslimov.vlad.gbspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbSpringApplication.class, args);
+	}
+
+}
